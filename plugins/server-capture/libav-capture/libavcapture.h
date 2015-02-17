c7e66db1ccbca4ee607a16345072fbcec0e9a202
hbirchtree <howy@ovi.com>
2015-02-17 16:01:19 +0100

    - Creating a C++-friendly class with the example

--- plugins/server-capture/libav-capture/libavcapture.h
+++ plugins/server-capture/libav-capture/libavcapture.h
@@ -3,12 +3,21 @@
 
 #include "../../../QStreamServer/capturinginterface.h"
 #include "libav-capture_global.h"
-#include <libavformat/avformat.h>
-#include <libavformat/avio.h>
-#include <libavcodec/avcodec.h>
-#include <libswscale/swscale.h>
-#include <libavcodec/avcodec.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+#include <libavutil/avassert.h>
+#include <libavutil/channel_layout.h>
+#include <libavutil/opt.h>
 #include <libavutil/mathematics.h>
+#include <libavutil/timestamp.h>
+#include <libavformat/avformat.h>
+#include <libswscale/swscale.h>
+#include <libswresample/swresample.h>
+
 #include <QCoreApplication>
 
 class LIBAVCAPTURESHARED_EXPORT Libavcapture : public CaptureInterface
@@ -33,8 +42,24 @@ public slots:
     void receiveUserInput(QString option){}
 private:
 
+    typedef struct OutputStream {
+        AVStream *st;
+
+        /* pts of the next frame that will be generated */
+        int64_t next_pts;
+        int samples_count;
+
+        AVFrame *frame;
+        AVFrame *tmp_frame;
+
+        float t, tincr, tincr2;
+
+        struct SwsContext *sws_ctx;
+        struct SwrContext *swr_ctx;
+    } OutputStream;
+
     //Constants
-    const QString internalPluginName = "LibAV Capture";
+    const QString internalPluginName = "FFmpeg Capture";
 
     const qint32 audio_bitrate = 64000;
     const qint32 audio_samplerate = 44100;
@@ -45,28 +70,31 @@ private:
     const qint32 video_w = 1600;
     const qint32 video_h = 900;
     const qint32 video_gopsize = 12;
+    const qint32 streamFramerate = 30;
     const AVPixelFormat streamPixFmt = PIX_FMT_YUV420P9;
-    const short sws_flags = SWS_BICUBIC;
-    const char streamFramerate = 30;
-
-    //AV variables
-    qint16 *a_samples;
-    quint16 *a_outbuf;
-    qint32 a_outbuf_sz;
-    qint32 a_i_framesz;
-
-    AVFrame *picture,*tmp_picture;
-    quint32 *v_outbuf;
-    qint32 v_framecount,v_outbuf_sz;
-
-    //AV variables
-    bool continueEncode = true; //used to keep the encoder alive, set to false when stopping
+    const qint16 swscaleFlags = SWS_BICUBIC;
+    const double streamDuration = 10.0;
 
     //AV functions
-    AVStream* add_audio_stream(AVFormatContext* oc, enum CodecID codec_id);
-    AVStream* add_video_stream(AVFormatContext* oc, enum CodecID codec_id);
-    void write_aframe(AVFormatContext *oc,AVStream *st);
-    void write_vframe(AVFormatContext *oc,AVStream *st);
+    int write_frame(AVFormatContext *fmt_ctx, const AVRational *time_base, AVStream *st, AVPacket *pkt);
+    void add_stream(OutputStream *ost, AVFormatContext *oc,
+                           AVCodec **codec,
+                           enum AVCodecID codec_id);
+    void close_stream(AVFormatContext *oc, OutputStream *ost):
+    //Audio
+    void open_audio(AVFormatContext *oc, AVCodec *codec, OutputStream *ost, AVDictionary *opt_arg);
+    AVFrame* alloc_audio_frame(enum AVSampleFormat sample_fmt,
+                                      uint64_t channel_layout,
+                                      int sample_rate, int nb_samples);
+    AVFrame* get_audio_frame(OutputStream *ost);
+
+
+    //Video
+    void open_video(AVFormatContext *oc, AVCodec *codec, OutputStream *ost, AVDictionary *opt_arg);
+    AVFrame* get_video_frame(OutputStream *ost);
+    void fill_yuv_image(AVFrame *pict, int frame_index,
+                               int width, int height);
+    int write_video_frame(AVFormatContext *oc, OutputStream *ost);
 };
 
 #endif // LIBAVCAPTURE_H
